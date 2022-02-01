de47d9dbfdcb7af1f04be7f4e01df15d00dac029
Vraksi <Jakobmetalv@gmail.com>
2022-02-01 14:50:51 +0100

    Bootstrap
    Navbar tæt på færdiggjort men har 1 bug, den står beskrevet i navbar.html

--- AngularFastfood/src/app/Components/navbar/navbar.component.ts
+++ AngularFastfood/src/app/Components/navbar/navbar.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  //TODO: the screen size will adjust down to the smallest size it can accomodate but i can for the life of me 
+  // just not get it show the content after dropdown the menu and collapse its just gone Example works so im missing something
+  // But it works for now so im just going with it
 
   //Login element of this part should probably be a popup of some kind so that.
+
   constructor() { }
 
   ngOnInit(): void {
